94c4c5c49963cdcd15e8e7f9b4597106a4aefd23
Aaron A <albon101@Aarons-MacBook-Pro.local>
2016-09-30 18:29:35 -0700

    fixed some of the logic

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friends=[
+
+{
+name: "Randall",
+photo: "http://www.talkoffamenetwork.com/wp-content/uploads/2014/10/cunningham-e1412359672720.jpg",
+scores: [
+"2",
+"5",
+"3",
+"1",
+"3",
+"3",
+"5",
+"2",
+"5",
+"4"
+]
+},
+{
+name: "Ricky",
+photo: "https://sportsoftheroundtable.files.wordpress.com/2012/08/eagles2.jpeg",
+scores: [
+"1",
+"3",
+"4",
+"2",
+"2",
+"1",
+"4",
+"2",
+"5",
+"5"
+]
+}
+
+module.exports = friends;
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,3 +1,7 @@
+var friendsDB = require('../data/friends.js');
+var path = require('path');
+
+module.exports = function (app) {
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'home.html'));
@@ -18,3 +22,4 @@ app.post('/api/friends', function (req, res) {
 	res.json(newFriend);
 });
 
+};
